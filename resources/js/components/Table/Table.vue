a95e9a3e44227e1860e8e6d7662dfb2308bd5875
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-05-07 20:54:47 +0700

    get data

--- resources/js/components/Table/Table.vue
+++ resources/js/components/Table/Table.vue
@@ -1,25 +1,44 @@
 <template>
   <div>
-    <router-view :isLoaded="isLoaded"/>
+    <router-view :isLoaded="isLoaded" />
   </div>
 </template>
 
 <script>
-import Table from '../../apis/Table'
+import Table from "../../apis/Table";
 export default {
-  data(){
+  data() {
     return {
       isLoaded: true,
-    }
+    };
   },
-  methods:{
-    tableIndex(){
-      Table
-    }
+  methods: {
+    tableIndex() {
+      this.isLoaded = false;
+      Table.index()
+        .then((res) => {
+          console.log(res);
+          this.isLoaded = true;
+        })
+        .catch((err) => {
+          if (err.response.status == 401) {
+            localStorage.removeItem("token");
+            this.$router.push({
+              name: "Login",
+              query: {
+                redirect: to.fullPath,
+              },
+            });
+          }
+          this.isLoaded = true;
+        });
+    },
   },
-}
+  mounted() {
+    this.tableIndex();
+  },
+};
 </script>
 
 <style>
-
 </style>
