2a8ec0db61c021b6e3b73d691f06a2fbf3c9f65e
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-05-07 20:48:46 +0700

    Truyền isLoaded

--- resources/js/components/Table/Table.vue
+++ resources/js/components/Table/Table.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <router-view/>
+    <router-view :isLoaded="isLoaded"/>
   </div>
 </template>
 
@@ -8,7 +8,7 @@
 export default {
   data(){
     return {
-      isLoaded: true,
+      isLoaded: false,
     }
   }
 }
