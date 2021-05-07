bc6dfd5b9eb545e8d172a6417e349ebed586c9ba
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-05-07 20:58:03 +0700

    send table's data

--- app/Http/Controllers/TableController.php
+++ app/Http/Controllers/TableController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Table;
 use Illuminate\Http\Request;
 
 class TableController extends Controller
@@ -13,7 +14,7 @@ class TableController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json(Table::all());
     }
 
     /**
