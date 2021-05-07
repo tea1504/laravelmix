522463524ca26c9e8b3ad5ed5a44c8d3185aecbb
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-05-07 10:09:31 +0700

    Thêm spinner và biến isLoaded

--- resources/js/components/Department/Department.vue
+++ resources/js/components/Department/Department.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
-    <router-view/>
+    <router-view />
+    <spinner v-if="!isLoaded" />
   </div>
 </template>
 
 <script>
+import Spinner from "../../views/Spinner.vue";
 export default {
-
-}
+  components: { Spinner },
+  data() {
+    return {
+      isLoaded: true,
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
